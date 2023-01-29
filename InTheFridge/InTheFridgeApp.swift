9050e030c9e2ad6f1e655219367edc02289cc44e
Aaron Wilson <awilson1995@yahoo.com>
2023-01-29 15:24:36 -0500

    Initial Commit

---
+++ InTheFridge/InTheFridgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  InTheFridgeApp.swift
+//  InTheFridge
+//
+//  Created by Aaron Wilson on 1/29/23.
+//
+
+import SwiftUI
+
+@main
+struct InTheFridgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
